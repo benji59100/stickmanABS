f14dff7e3114b782692b7c6fd921165d06ff9858
VDB benjamin <benji59100@gmail.com>
2016-07-13 15:17:52 +0200

    test

--- classe/cCell.php
+++ classe/cCell.php
@@ -4,8 +4,15 @@ class cCell
 {
 	public $iPosX; //positionX de la cellule
 	public $iPosY; //position Y de la cellule
-	public $aovItems = array(); //Items présent sur la cellule
-	public $aoStickmen=array(); //Stickmen présent sur la cellule
+	public $aovItems =	array(); //Items présent sur la cellule
+	public $aoStickmen	= array(); //Stickmen présent sur la cellule
+
+	function __contruct($a,$b)
+	{
+		$this->iPosX=$a;
+		$this->iPosY=$b;
+
+	}
 }
 
 
