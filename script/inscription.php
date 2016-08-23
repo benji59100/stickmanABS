f1cf3828cfd70be7946e40de893043e0d49dbd6b
benji59100 <benji59100@gmail.com>
2016-08-23 10:14:05 +0200

    ajout ajax

--- script/inscription.php
+++ script/inscription.php
@@ -1,28 +1,21 @@
 <?php 
-include("../includes/ini.php");
+//include("../includes/ini.php");
 
 if(isset($_POST['username'],$_POST['password'],$_POST['pseudo'],$_POST['email']))
 {
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	$pseudo = $_POST['pseudo'];
-	$email = $_POST['email'];
-}
-
-function verifLogin($username,$email)
+	if(($_POST['username'] !== '') && ($_POST['password'] !== '') && ($_POST['pseudo'] !== '') && ($_POST['email'] !== '')) 
+	{
+        $reponse = 'ok';
+    } 
+    else 
+    {
+        $reponse = 'Les champs sont vides';
+    }
+} 
+else 
 {
-	//Vérification si le login n'est pas déjà utilisé.
-	$query = "SELECT * from compte WHERE Username = $username OR Email = $email";
-	$verif = $connexion->execute($query);
-	$nbrows = count($verif);
-	if($nbrows != 0 )
-	{
-		
-	}
-	else
-	{
-
-	}
+    $reponse = 'Tous les champs ne sont pas parvenus';
 }
-
+ 
+echo json_encode(['reponse' => $reponse]);
 ?>
