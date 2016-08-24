36d45eb441bca28eca14ca72f64cc5ceb7e112e4
Funkeal <clydeponteville@hotmail.fr>
2016-08-24 10:31:20 +0200

    modif magasin

--- views/shop.php
+++ views/shop.php
@@ -1,7 +1,12 @@
 <html>
     <head>
-        <link rel="stylesheet" href="css/shop.css">
+        
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/shop.css">
+        <link rel="stylesheet" href="css/buttons.css">
+        <link rel="stylesheet" href="css/base.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/vicons-font.css">
     </head>
     <body>
         <div id="shop">
@@ -10,7 +15,9 @@
                 <div  class="item col-md-3 col-md-offset-1">
                     <h2>Name</h2>
                     <img  src="../img/shop/red_potion.png" alt="red_potion">
-                    <button>Acheter</button>
+                    <div class="box ">
+                        <button class="button button--naira button--border-medium button--round-l button--text-thick button--inverted"><i class="button__icon glyphicon glyphicon-shopping-cart">10</i><span>Acheter</span></button>
+                    </div>
                 </div>
                 <div  class="item col-md-3 col-md-offset-1">
                     <h2>Name</h2>
