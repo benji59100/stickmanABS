084e83aeec11a7bea75693a663cac503c555c483
gorski62 <gorskidu62@gmail.com>
2016-08-24 10:23:09 +0200

    modif profil et bdd

--- views/profil.php
+++ views/profil.php
@@ -9,68 +9,76 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="apple-touch-icon" href="../apple-touch-icon.png">
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
         <style>
             body {
                 padding-top: 50px;
                 padding-bottom: 20px;
             }
         </style>
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="../css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="../css/main.css">
+        <link rel="stylesheet" href="/css/profil.css">
         <link href='https://fonts.googleapis.com/css?family=Kalam' rel='stylesheet' type='text/css'> <!-- google font font-family: 'Kalam', cursive;-->
 
-        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <script src="../js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">StickMan ABS</a>
+        <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">StickMan ABS</a>
           
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <form id="form-disconnect" class="navbar-form navbar-right" action="script/deconnexion.php" method="post" role="form">
-            <button type="submit" class="btn btn-success">Disconnect</button>
-          </form>
-        </div><!--/.navbar-collapse -->
-      </div>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <form id="form-deconnexion" class="navbar-form navbar-right" action="script/deconnexion.php" method="post" role="form">
+                <button type="submit" class="btn btn-success">Disconnect</button>
+              </form>
+            </div><!--/.navbar-collapse -->
+        </div><!-- /.container-fluid -->
     </nav>
+    <nav class="navbar navbar-inverse navbar-lower" role="navigation">
+        <div class="container">
+            <div class="span10">
+                <div class="row">
+                    <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="filler"></div>
+        </div>
+    </div>
+        
+    
     <!-- Main jumbotron for a primary marketing message or call to action -->
     
     <div class="container">
       <!-- Example row of columns -->
-      <p style="text-align: center;"><img src="img/banniere.png" name="img" id="img"></p>
-      <div class="form-group">
+      
         
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
+        
+        
+        
+        
+        
+        
+        
+        
+        
       <hr>
 
       <footer>
@@ -78,12 +86,12 @@
       </footer>
     </div> <!-- /container -->      
     <script   src="https://code.jquery.com/jquery-3.1.0.js"   integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk="   crossorigin="anonymous"></script>
-    <script src="js/ajax.js"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="../js/ajax.js"></script>
+    <script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
-    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="../js/vendor/bootstrap.min.js"></script>
 
-    <script src="js/main.js"></script>
+    <script src="../js/main.js"></script>
 
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
     <script>
