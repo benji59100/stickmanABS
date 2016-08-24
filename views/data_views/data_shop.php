d5da59390c0e3191e223206d51c5005dd7274114
Funkeal <clydeponteville@hotmail.fr>
2016-08-24 12:16:50 +0200

    modif datashop

--- views/data_views/data_shop.php
+++ views/data_views/data_shop.php
@@ -5,12 +5,13 @@
     $data_shop->execute();
     
     $resultat = $data_shop->fetchAll();
-    $i = 0;
-        foreach ($resultat as $row) {
-           $objet = new item(Name);
-           $item[$i] = $objet;
-            
-        
-    }
+    var_dump($resultat);
+//    $i = 0;
+//        foreach ($resultat as $row) {
+//           $objet = new item(Name);
+//           $item[$i] = $objet;
+//            
+//        
+//    }
 
 ?>
