8383017f9a8b437b42fd632f8acbbf877e5a3b96
Funkeal <clydeponteville@hotmail.fr>
2016-08-24 08:52:40 +0200

    modif vue shop

--- views/shop.php
+++ views/shop.php
@@ -1,6 +1,39 @@
-<div id="shop">
-    <h1>ICI LE MAGASIN</h1>
-    <div id="item">
-        <img src="../img/shop/red_potion.png" alt="red_potion">
-    </div>
-</div>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/shop.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+        <div id="shop">
+            <h1>ICI LE MAGASIN</h1>
+            <div id="container" class="row">
+                <div  class="item col-md-3 col-md-offset-1">
+                    <h2>Name</h2>
+                    <img  src="../img/shop/red_potion.png" alt="red_potion">
+                    <button>Acheter</button>
+                </div>
+                <div  class="item col-md-3 col-md-offset-1">
+                    <h2>Name</h2>
+                    <img  src="../img/shop/force_potion.png" alt="force_potion">
+                </div>
+                <div  class="item col-md-3 col-md-offset-1">
+                    <h2>Name</h2>
+                    <img  src="../img/shop/blue_potion.png" alt="blue_potion">
+                </div>
+                <div  class="item col-md-3 col-md-offset-1">
+                    <h2>Name</h2>
+                    <img  src="../img/shop/red_potion.png" alt="red_potion">
+                </div>
+                <div  class="item col-md-3 col-md-offset-1">
+                    <h2>Name</h2>
+                    <img  src="../img/shop/force_potion.png" alt="force_potion">
+                </div>
+                <div  class="item col-md-3 col-md-offset-1">
+                    <h2>Name</h2>
+                    <img  src="../img/shop/blue_potion.png" alt="blue_potion">
+                </div>
+                
+            </div>
+        </div>
+    </body>
+</html>
