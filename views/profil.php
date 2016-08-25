37370cac2b3279de935bd6f678d721549e76e154
gorski62 <gorskidu62@gmail.com>
2016-08-25 14:50:04 +0200

    profil

--- views/profil.php
+++ views/profil.php
@@ -20,7 +20,7 @@
         </style>
         <link rel="stylesheet" href="../css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="../css/main.css">
-        <link rel="stylesheet" href="/css/profil.css">
+        <link rel="stylesheet" href="css/profil.css">
         <link href='https://fonts.googleapis.com/css?family=Kalam' rel='stylesheet' type='text/css'> <!-- google font font-family: 'Kalam', cursive;-->
 
         <script src="../js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
@@ -70,7 +70,183 @@
     <div class="container">
       <!-- Example row of columns -->
       
+        <div class="box-profil">
         
+            <div class="stickman-picture">
+                <!-- box img+pseudo !-->
+                <img src="../img/profil/stickman.jpg" class="stickman">
+                <div class="pseudo">
+                    <!-- recherche de pseudo dans la bdd !-->
+                </div>
+            </div>
+            <div class="stat">
+                <div class="left-side-stat">
+                    <!-- coté gauche, récuperer chaque info de la bdd !-->
+                    <div id="life">
+                        Life:
+                    </div>
+                    <div id="actionpt">
+                        Action Point:
+                    </div>
+                    <div id="movept">
+                        Move Point:
+                    </div>
+                </div>
+                <div class="right-side-stat">
+                    <!-- coté droit !-->
+                    <div id="points">
+                        Points:
+                    </div>
+                    <div id="??">
+                        not defined yet!!
+                    </div>
+                    <div id="strengh">
+                        Strengh:
+                    </div>
+                </div>
+                <div class="bot-side">
+                    <!-- coté bas !-->
+                    <div class="victories">
+                        <!-- victories!-->
+                        Victories:
+                    </div>
+                    <div class="defeats">
+                        <!-- defeats!-->
+                        Defeats:
+                    </div>
+                </div>
+            </div>
+            <div class="inventory ">
+                <div class="case case0" id="0">
+                0
+                
+                </div>
+                <div class="case case1" id="1">
+                1
+                
+                </div>
+                <div class="case case2" id="2">
+                
+                2
+                </div>
+                <div class="case case3" id="3">
+                
+                3
+                </div>
+                <div class="case case4" id="4">
+                
+                4
+                </div>
+                <div class="case case5" id="5">
+                
+                5
+                </div>
+                <div class="case case6" id="6">
+                6
+                
+                </div>
+                <div class="case case7" id="7">
+                
+                7
+                </div>
+                <div class="case case8" id="8">
+                8
+                
+                </div>
+                <div class="case case9" id="9">
+                
+                9
+                </div>
+                <div class="case case10" id="10">
+                
+                10
+                </div>
+                <div class="case case11" id="11">
+                
+                11
+                </div>
+                <div class="case case12" id="12">
+                
+                12
+                </div>
+                <div class="case case13" id="13">
+                13
+                
+                </div>
+                <div class="case case14" id="14">
+                14
+                
+                </div>
+                <div class="case case15" id="15">
+                15
+                
+                </div>
+                <div class="case case16" id="16">
+                16
+                
+                </div>
+                <div class="case case17" id="17">
+                
+                17
+                </div>
+                <div class="case case18" id="18">
+                18
+                
+                </div>
+                <div class="case case19" id="19">
+                19
+                
+                </div>
+                <div class="case case20" id="20">
+                20
+                
+                </div>
+                <div class="case case21" id="21">
+                21
+                
+                </div>
+                <div class="case case22" id="22">
+                22
+                
+                </div>
+                <div class="case case23" id="23">
+                23
+                
+                </div>
+                <div class="case case24" id="24">
+                24
+                
+                </div>
+                <div class="case case25" id="25">
+                25
+                
+                </div>
+                <div class="case case26" id="26">
+                26
+                
+                </div>
+                <div class="case case27" id="27">
+                27
+                
+                </div>
+                <div class="case case28" id="28">
+                28
+                
+                </div>
+                <div class="case case29" id="29">
+                29
+                
+                </div>
+                <div class="case case30" id="30">
+                30
+                
+                </div>
+                <div class="case case31" id="31">
+                31
+                
+                </div> 
+            </div>
+        </div>
         
         
         
